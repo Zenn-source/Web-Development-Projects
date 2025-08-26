c52ac7c8d5b19b4c5bf17c4e276c0bce2c717300
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-08-26 15:00:25 +0800

    feat: add password-protected route with middleware authentication

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,37 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function checkAuth(req, res, next) {
+  if (req.body.password === "ILoveProgramming") {
+    next();
+  } else {
+    res.status(401).send("Not Authorized");
+    console.log(`Wrong password: ${req.body.password}`);
+  }
+}
+
+app.post("/secrets", checkAuth, (req, res) => {
+  res.sendFile(__dirname + "/public/secret.html");
+  console.log("User Authorized.");
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
