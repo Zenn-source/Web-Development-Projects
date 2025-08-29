fed6ef7dbd832614a4f66cd61564a0c65d97854a
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-08-29 21:00:08 +0800

    Passing Data to EJS Templates

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -6,10 +6,20 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const firstName = req.body["fName"].length;
+  const lastName = req.body["lName"].length;
+  const totalLetters = firstName + lastName;
+  res.render("index.ejs", {
+    numberOfLetters: totalLetters,
+  });
+});
+  
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
