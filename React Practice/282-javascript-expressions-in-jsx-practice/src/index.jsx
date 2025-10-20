68d0df978298b66b7cba26df545b43100a77e736
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-10-20 20:32:50 +0800

    JS expressions in React

--- React Practice/282-javascript-expressions-in-jsx-practice/src/index.jsx
+++ React Practice/282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -7,8 +7,17 @@
 //Created by Angela Yu.
 //Copyright 2019.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom";
+
+const firstName = "Mackenzie";
+const lastName = "Iguiron";
+const year = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <h1>Created by {firstName} {lastName}</h1>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById('root')
+);
