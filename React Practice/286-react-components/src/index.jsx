956cfdd4ffa05690ced33cae7c76763e8e08a3be
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-10-24 08:59:19 +0800

    Add components

--- React Practice/286-react-components/src/index.jsx
+++ React Practice/286-react-components/src/index.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List"
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
