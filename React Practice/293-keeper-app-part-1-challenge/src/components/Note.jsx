86831ca2b6bca5e928a8767db061e5e938cc7e4d
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-10-26 08:59:32 +0800

    Display a note in page

---
+++ React Practice/293-keeper-app-part-1-challenge/src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Cook dinner</h1>
+      <p>Ingredients</p>
+      <ul>
+        <li>Garlic</li>
+        <li>Soy Sauce</li>
+        <li>Pork</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Note;
