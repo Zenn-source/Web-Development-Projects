9da907b005f0764a67dee9dbad8263e921ccc0d7
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-11-04 15:40:34 +0800

    Use ternary operator in rendering conditionally

--- React Practice/303-conditional-rendering/src/components/App.jsx
+++ React Practice/303-conditional-rendering/src/components/App.jsx
@@ -3,18 +3,18 @@ import Login from "./Login";
 
 let isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />
-  }
-}
+// function renderConditionally() {
+//   if (isLoggedIn) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />
+//   }
+// }
 
 function App() {
-  return <div className="container">
-    {renderConditionally()}
-  </div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
 }
 
 export default App;
