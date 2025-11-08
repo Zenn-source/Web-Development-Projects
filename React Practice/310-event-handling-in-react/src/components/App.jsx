0ab44598c7c1414822568fa4590dc9a9d94fbb24
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-11-08 12:04:32 +0800

    Finish event handling module

--- React Practice/310-event-handling-in-react/src/components/App.jsx
+++ React Practice/310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+
+  const [isHovered, setHovered] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setHovered(true);
+  }
+
+  function handleMouseOut() {
+    setHovered(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{
+          backgroundColor: isHovered ? "black" : "white",
+          color: isHovered ? "white" : null,
+        }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={handleClick}>
+        Submit
+      </button>
     </div>
   );
 }
